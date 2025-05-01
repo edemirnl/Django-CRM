02d67a7271fae02fdcf7e92877264f1c9c7b22bf
RoulaArab <roula.arab95@gmail.com>
2025-05-01 14:07:43 +0200

    steps for run the project

--- common/views.py
+++ common/views.py
@@ -1,7 +1,8 @@
 import json
 import secrets
 from multiprocessing import context
-from re import template
+#from re import template
+import re
 
 import requests
 from django.contrib.auth.base_user import BaseUserManager
